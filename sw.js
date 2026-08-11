fb9e58f54f314f0170edbd5865bb6853a4a2e730
cuentatrabajo86-arch <cuentatrabajo86@gmail.com>
2026-08-11 18:17:09 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
    clientes seguirán viendo la app vieja.
    =========================================================== */
 
-const VERSION = 'nexbit-v40';
+const VERSION = 'nexbit-v43';
 const SHELL   = VERSION + '-shell';   // la app
 const EXTERNO = VERSION + '-externo'; // fuentes y librerías
 
